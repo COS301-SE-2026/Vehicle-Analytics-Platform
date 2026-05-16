6c3316298f83293e07cf3c5d37114a081a81f6af
ZiphoZinhle <u22555740@tuks.co.za>
2026-05-16 23:31:36 +0200

    feat: add vehicle service for KPI, location and fleet data fetching

---
+++ frontend/src/services/vehicleService.jsx
@@ -0,0 +1,57 @@
+// src/services/vehicleServices.js
+//TODO: replace mock data with real API call when auth is merged 
+// Real calls will look like this:
+// const res = await fetch(`${import.meta.env.VITE_API_URL}/dashboard/kpis`, {
+// headers: {Authorization: `Bearer ${tocken}`}
+// })
+
+export async function getKPIs() {
+    // Matches GET /dashboard/kpis response shape exactly
+    return{
+        totalVehicles: 15,
+        activeVehicles: 11,
+        averageSpeed: 54,
+        alertsToday: 2, // need to look into this alert situation -> can we actually return alerts without crashing?
+        lastUpdated: new Date().toISOString(),
+    }
+}
+
+export async function getVehicleLocations(){
+    // Matches GET /dashboard/kpis response shape exactly
+    return {
+        timestamp : new Date().toISOString,
+        vehicles: [
+            { id: '1000', lat: -27.98763, lng: 28.37466, speed: 65, status: 'active' },
+            { id: '1001', lat: -28.12345, lng: 28.56789, speed: 42, status: 'active' },
+            { id: '1002', lat: -27.75432, lng: 28.12345, speed: 0,  status: 'idle' },
+            { id: '1003', lat: -28.34521, lng: 28.89012, speed: 78, status: 'active' },
+            { id: '1004', lat: -27.65432, lng: 28.45678, speed: 0,  status: 'offline' },
+            { id: '1005', lat: -28.56789, lng: 28.23456, speed: 55, status: 'active' },
+        ],
+    }
+}
+
+export async function getAlerts() {
+    // Matches GET /dashboard/alerts response shape exactly
+    return {
+        total: 2,
+        alerts: [
+            {
+              id: 'alert_001',
+              vehicleId: '1000',
+              type: 'speeding',
+              severity: 'high',
+              message: 'Vehicle exceeded speed limit: 95 km/h in 80 zone',
+              timestamp: new Date(Date.now() - 5 * 60000).toISOString(),
+            },
+            {
+              id: 'alert_002',
+              vehicleId: '1003',
+              type: 'speeding',
+              severity: 'medium',
+              message: 'Vehicle exceeded speed limit: 85 km/h in 80 zone',
+              timestamp: new Date(Date.now() - 111 * 60000).toISOString(),
+            },
+        ],
+    }
+}

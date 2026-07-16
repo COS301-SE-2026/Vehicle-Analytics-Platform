aad4bffc25fdebb7b47f09b1994226655e485efb
Marchant <u21549232@tuks.co.za>
2026-07-16 03:02:37 +0200

    frontend deployment setup

---
+++ frontend/frontendstack.ts
@@ -0,0 +1,11 @@
+import * as path from 'path';
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
+import * as origins from 'aws-cdk-lib/aws-cloudfront-origins';
+import * as s3deploy from 'aws-cdk-lib/aws-s3-deployment';
+
+export interface FrontendStackProps extends cdk.StackProps {
+  envName: string;
+}

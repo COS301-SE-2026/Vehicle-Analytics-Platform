df104f334e94cbaecbe4fd2d5c35440fd57b32c6
u23770912 <u23770912@tuks.co.za>
2026-07-17 13:30:09 +0200

    ??

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,72 +1,23 @@
 
 
 const express = require('express');
-
-
-
-
 const cors = require('cors');
-
-
-
 const helmet = require('helmet');
-
-
-
 const rateLimit = require('express-rate-limit');
-
-
-
 const authRoutes = require('./routes/auth');
-
-
-
 const vehicleRoutes = require('./routes/vehicles');
-
-
-
 const dashboardRoutes = require('./routes/dashboard');
-
-
-
 const adminRoutes = require('./routes/admin');
-
 //const safetyRoutes = require('./routes/safety');
-
-
 const safetyRoutes = require('./routes/safety');
-
-
 const geofenceRoutes = require('./routes/geofence');
-
 // const fleetAnalyticsRoutes = require('./routes/fleetAnalytics');
-
 // const geofenceRoutes = require('./routes/geofence');
-
-
-
 const app = express();
-
-
-
 const limiter = rateLimit({
-
-
-
   windowMs: 15*60*1000,
-
-
-
-
   max: 1000,
-
-
-
-
   message: 'Too many requests from this IP, please try again later.',
-
-
-
 });
 
 
@@ -136,13 +87,7 @@ app.use('/api/admin', adminRoutes);
 
 
 app.use('/api/safety', safetyRoutes);
-
-
-
-//app.use('/api/trips', tripRoutes);
-
-
-
+app.use('/api/trips', tripRoutes);
 app.use('/api/geofences',geofenceRoutes);
 
 // app.use('/api/fleet', fleetAnalyticsRoutes);

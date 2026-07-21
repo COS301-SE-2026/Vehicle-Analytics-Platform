0972eb7cdfbdc1f51efc5faa5283fe381c1fbfc9
ZiphoZ <u22555740@tuks.co.za>
2026-07-21 21:30:11 +0200

    feat: added files for the different parts of the landing page and implemented the index file

---
+++ frontend/src/pages/landing/index.jsx
@@ -0,0 +1,15 @@
+import Navbar from './Navbar';
+import Hero from './Hero';
+import Sections from './Sections';
+import Footer from './Footer';
+
+export default function LandingPage() {
+    return (
+        <>
+            <Navbar />
+            <Hero />
+            <Sections />
+            <Footer />
+        </>
+    );
+}

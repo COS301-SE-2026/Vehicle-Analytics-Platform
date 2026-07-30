c7128c6f23b813321470285ac769f097dcc5660c
Kwanele-P <anepha157@gmail.com>
2026-07-30 11:31:28 +0200

    fix the mock to support map load and playback for tests

--- frontend/__mocks__/mapbox-gl.js
+++ frontend/__mocks__/mapbox-gl.js
@@ -3,6 +3,11 @@ module.exports = {
   Map: jest.fn().mockImplementation(() => ({
     addControl: jest.fn(),
     remove: jest.fn(),
+    on: jest.fn((event, callback) => {
+      if (event === 'load') callback()
+    }),
+    addSource: jest.fn(),
+    addLayer: jest.fn(),
   })),
   NavigationControl: jest.fn(),
   Marker: jest.fn().mockImplementation(() => ({
